81346d767983eaf9dcf6029574b046f35c4a64a0
zaidansari9243 <zaidansari0924300@gmail.com>
2026-08-09 13:13:52 +0530

    Time: 3 ms (8.48%), Space: 12.1 MB (25.7%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -4,12 +4,12 @@ public:
         int m = strs.size();
         if(m==1) return strs[0];
         sort(strs.begin(),strs.end());
-        string p= strs[0];
-        string q= strs[m-1];
+        string p= strs[0]; //on first word of string
+        string q= strs[m-1];// on last word of string
         string s="";
         for(int i=0;i<(min(p.size(),q.size()));i++){
             if(p[i]==q[i]){
-                s += p[i];
+                s += p[i]; //+ operator use to add in string s
             }
             else return s;
         }
