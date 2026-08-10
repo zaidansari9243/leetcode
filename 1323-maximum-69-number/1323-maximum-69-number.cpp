695b233858923751d39a688ff41a34eb3a9a98cc
zaidansari9243 <zaidansari0924300@gmail.com>
2026-08-10 10:35:19 +0530

    Time: 0 ms (100%), Space: 8.1 MB (40.25%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+     int max=0;
+     string str = to_string(num);
+     for(int i=0;i<str.length();i++){
+        if(str[i]=='6'){
+            str[i]='9';   
+            break;
+        }
+     }
+     max = stoi(str);
+     return max;
+    }
+    
+};
