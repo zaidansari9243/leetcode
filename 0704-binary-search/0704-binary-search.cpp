ee7430392df1bbe8b4c8e15e36a2e1e1f060f0b7
zaidansari9243 <zaidansari0924300@gmail.com>
2026-08-20 10:25:48 +0530

    Time: 0 ms (100%), Space: 31.3 MB (81.65%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -5,7 +5,7 @@ public:
         int lo = 0 ;
         int hi = n-1;
         while(lo<=hi){
-            int mid = (lo + hi)/2;
+            int mid = lo + (hi - lo )/2;
             if(nums[mid] == target ) return mid ;
             else if (nums[mid] <target) lo = mid + 1;
             else hi = mid - 1;
