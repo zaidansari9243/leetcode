ceb330ec6b4eec6fd5ef387442b9674ffd342390
zaidansari9243 <zaidansari0924300@gmail.com>
2026-09-07 20:01:27 +0530

    Time: 0 ms (100%), Space: 8.6 MB (10.62%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+       int sum = 0;
+        int i =1;
+        while(i<=n){
+         if(i%3==0 || i%5==0 || i%7==0) sum += i;
+         i++;
+        }
+        return sum;
+    }
+};
